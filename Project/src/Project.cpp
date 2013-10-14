66dcd2f9d92c9b6e9e513f5dd1b36482724613bf
VincentDS <de_vinnie111_rulezzz@hotmail.com>
2013-10-14 22:56:12 +0200

    Correct argument passing

--- Project/src/Project.cpp
+++ Project/src/Project.cpp
@@ -22,9 +22,27 @@ void asker(ifstream& specification, ofstream& answers) {
 }
 
 
-int main() {
-	ifstream specification("src/enquetespecificatie.txt");
-	ofstream answers("src/antwoordbestand.txt");
-	asker(specification, answers);
+bool correctarguments(int argc, const char *argv[]) { //checks if the 2 correct files are passed.
+	if (argc != 3) {
+		cout << "invalid amount of arguments" <<endl;
+		return false;
+	}
+	else if ((strcmp(argv[1], "enquetespecificatie.txt") != 0) ||
+			(strcmp(argv[2], "antwoordbestand.txt") != 0) ) {
+		cout << "invalid arguments passed" <<endl;
+		return true;
+	}
+	else {
+		cout << "correct arguments passed!" <<endl;
+		return true;
+	}
+}
+
+
+int main(int argc, const char *argv[]) {
+	correctarguments(argc, argv);
+//	ifstream specification(argv[1]);
+//	ofstream answers(argv[2]);
+//	asker(specification, answers);
 	return 0;
 }
