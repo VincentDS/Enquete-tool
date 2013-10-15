5e7f921e82417b17dcc769d7ae34bc9e334b8c92
VincentDS <de_vinnie111_rulezzz@hotmail.com>
2013-10-15 11:16:45 +0200

    All functions in one cpp file

--- Project/src/asker.cpp
+++ Project/src/asker.cpp
@@ -9,7 +9,34 @@
 #include <fstream> //for file-access
 #include <string>
 #include <sstream>
+#include "asker.h"
 using namespace std;
+
+
+bool correctarguments(int argc, const char *argv[]) { //checks if the 2 correct files are passed.
+	if (argc != 3) {
+		cout << "invalid amount of arguments" <<endl;
+		return false;
+	}
+	else if ((strcmp(argv[1], "enquetespecificatie.txt") != 0) ||
+			(strcmp(argv[2], "antwoordbestand.txt") != 0) ) {
+		cout << "invalid arguments passed" <<endl;
+		return false;
+	}
+	else {
+		return true;
+	}
+}
+
+
+void intro(const char *argv[]) {
+	cout << "vdeschut@vub.ac.be " << "taak1 " << argv[1] << " " << argv[2] << endl;
+}
+
+void outro(const char *argv[]) {
+	cout << "Antwoordbestand weggeschreven naar \"" << argv[2] << "\"." << endl;
+}
+
 
 int numberofsteps(string stepsstring) {
 	string tmp;  // temporary string to insert "STEPS" before we can acquire the steps integer.
@@ -27,6 +54,7 @@ void asker(ifstream& specification, ofstream& answers) {
 	string id;
 	string steps;
 	string questions;
+	string newline("\n");
 
 	getline(specification, version);
 	getline(specification, id);
@@ -38,5 +66,24 @@ void asker(ifstream& specification, ofstream& answers) {
 
 	cout << "Deze enquete bestaat uit " << numberofsteps(steps) << " vragen." << endl << endl;
 
+	answers << id;
+	answers << newline;
+
+	cout << questions << endl;
+
+
 	cout << "Bedankt voor je deelname." << endl;
+}
+
+
+int main(int argc, const char *argv[]) {
+	if (correctarguments(argc, argv)) {
+		ifstream specification(argv[1]);
+		ofstream answers(argv[2]);
+
+		intro(argv); //prints intro with the filenames
+		asker(specification, answers);
+		outro(argv); //prints outro with the filename to which the answers are passed
+	}
+	return 0;
 }
--- Project/src/asker.h
+++ Project/src/asker.h
@@ -8,6 +8,9 @@
 
 #ifndef ASKER_H_
 
+bool correctarguments(int argc, const char *argv[]);
+void intro(const char *argv[]);
+void outro(const char *argv[]);
 int numberofsteps(std::string stepsstring);
 void asker(std::ifstream& specification, std::ofstream& answers);
 
